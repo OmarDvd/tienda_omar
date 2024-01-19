fff5e19dac1909f4b6c051de9ba0321718171dd4
OmarDvd <71876725+OmarDvd@users.noreply.github.com>
2024-01-19 14:24:26 +0100

    Update comprobarAcceso.js

--- comprobarAcceso.js
+++ comprobarAcceso.js
@@ -3,21 +3,25 @@ si la llave no es true, es porque no ha habido token de identificacion de vuelta
 contra la api de la fakestore, por tanto, nos lleva de nuevo al login que el index.html. Si es true la llave
 entonces guardamos el nombre de usuario y lo mostramos en la barra de navegacion de nuestras paginas, en el li con clase nonbreUsuario*/
 
+//para evitar que existiendo el token, podamos entrar habiendo deslogeado en populares.html, le decimos que tiene que existir también username
+// si no, nos vamos al index. Esto es para logearnos por segunda vez sin haber cerrado el navegador. Así no podemos entrar nunca sin logearnos
+
 if (!sessionStorage.getItem('llave')) {
     window.location.href = 'index.html';
 } else {
-    console.log(sessionStorage.getItem('username'));
-    var elementos = document.querySelectorAll(".nombreUsuario");
-
-    elementos.forEach(element => {
-        element.textContent = sessionStorage.getItem('username');
-    });
+    if(!sessionStorage.getItem('username')){
+        window.location.href = 'index.html';
+    }else{
+        console.log(sessionStorage.getItem('username'));
+        var elementos = document.querySelectorAll(".nombreUsuario");
+    
+        elementos.forEach(element => {
+            element.textContent = sessionStorage.getItem('username');
+        });
+    }
+    
 
 }
-
-
-
-
 
 
 
